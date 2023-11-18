f90e7c30b046dfa7cd739ae09734de933e6ba20c
nahuelsosa2105 <nahuelsosa2105@gmail.com>
2023-11-18 19:03:45 -0300

    Grabado exitoso de nuevo empleado

---
+++ clsArchivo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prySosaTP
+{
+    internal class clsArchivo
+    {
+        public string nombreArchivo = "NuevoEmpleado.txt";
+
+        public void Grabar(string datosConcatenados)
+        {
+            StreamWriter AD = new StreamWriter(nombreArchivo,true);
+            AD.WriteLine(datosConcatenados);
+            AD.Close();
+
+        }
+    }
+}
--- frmRegistro.cs
+++ frmRegistro.cs
@@ -23,10 +23,70 @@ namespace prySosaTP
             frmPrincipal principal = new frmPrincipal();
             principal.Show();
         }
+        clsArchivo grabar = new clsArchivo();
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
+            if (txtNombre.Text == "")
+            {
+                txtNombre.Focus();
+                MessageBox.Show("Campo Nombre vacio");
+            }
+            else
+            {
+                if (txtApellido.Text == "")
+                {
+                    txtApellido.Focus();
+                    MessageBox.Show("Campo Apellido Vacio");
+                }
+                else
+                {
+                    if (dtpFechaNacimiento == null)
+                    {
+                        dtpFechaNacimiento.Focus();
+                        MessageBox.Show("Seleccione una fecha valida");
+                    }
+                    else
+                    {
+                        if (txtDireccion.Text == "")
+                        {
+                            txtDireccion.Focus();
+                            MessageBox.Show("Campo Direccion vacio");
+                        }
+                        else
+                        {
+                            if (txtCiudad.Text == "")
+                            {
+                                txtCiudad.Focus();
+                                MessageBox.Show("Campo Ciudad Vacio");
+                            }
+                            else
+                            {
+                                if (txtNumero.Text == "")
+                                {
+                                    txtNumero.Focus();
+                                    MessageBox.Show("Campo Numero Vacio");
+                                }
+                                else
+                                {
+                                    string[] datosEmpleado = new string[] { txtNombre.Text, txtApellido.Text, dtpFechaNacimiento.Value.Date.ToString("dd-MM-yyyy"), txtDireccion.Text, txtCiudad.Text, txtNumero.Text };
+                                    string datosConcatenados = string.Join(";", datosEmpleado);
+                                    grabar.Grabar(datosConcatenados);
 
+                                    txtNombre.Clear();
+                                    txtApellido.Clear();
+                                    txtDireccion.Clear();
+                                    txtCiudad.Clear();
+                                    txtNumero.Clear();
+                                    MessageBox.Show("Empleado cargado correctamente");
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
 }
